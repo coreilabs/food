6a491503d8e49bc8166ab5db2d9898204c2df115
Marco Aurélio Silva <coreilabsbr@gmail.com>
2023-11-01 17:58:39 -0300

    aula 37

--- app/Views/Admin/Usuarios/show.php
+++ app/Views/Admin/Usuarios/show.php
@@ -22,22 +22,24 @@
 
 <div class="row">
            
-            <div class="col-lg-12 grid-margin stretch-card">
-              <div class="card">
-                <div class="card-body">
+<div class="col-lg-6 grid-margin stretch-card">
+  <div class="card">
+    <div class="card-body">
+      <h2 class="card-title"><?= esc($titulo) ?></h2>
+      <hr>
+      <p class="card-text"> <span class="font-weight-bold">Nome: </span> <?= esc($usuario->nome)?></p>
+      <p class="card-text"> <span class="font-weight-bold">Email: </span> <?= esc($usuario->email)?></p>
+      <p class="card-text"> <span class="font-weight-bold">Ativo: </span> <?= ($usuario->ativo ? "Sim" : "Não" )?></p>
+      <p class="card-text"> <span class="font-weight-bold">Perfil: </span> <?= ($usuario->is_admin ? "Administrador" : "Cliente" )?></p>
+      <p class="card-text"> <span class="font-weight-bold">Criado: </span> <?= $usuario->criado_em ?></p>
+      <p class="card-text"> <span class="font-weight-bold">Atualizado: </span> <?= $usuario->criado_em ?></p>
 
-                  <h2 class="card-title"><?= esc($titulo) ?></h2>
-                  <hr>
 
-             
-                  <p class="card-text"><?= esc($usuario->nome)?></p>
-                
 
-              
 
-                </div>
-              </div>
-            </div>
+    </div>
+  </div>
+</div>"
             
 
           </div>
