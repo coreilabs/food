11a64a70bc1376f418a1844cc5d6dbc2538a0572
Marco Aurélio Silva <coreilabsbr@gmail.com>
2023-11-02 20:14:04 -0300

    aula 51 - validando CPF

--- app/Models/UsuarioModel.php
+++ app/Models/UsuarioModel.php
@@ -20,7 +20,7 @@ class UsuarioModel extends Model
     protected $validationRules = [
         'nome'     => 'required|max_length[120]|alpha_numeric_space|min_length[3]',
         'email'        => 'required|max_length[254]|min_length[4]|valid_email|is_unique[usuarios.email]',
-        'cpf'        => 'required|exact_length[14]|is_unique[usuarios.cpf]',
+        'cpf'        => 'required|exact_length[14]|is_unique[usuarios.cpf]|validaCpf',
         'password'     => 'required|max_length[255]|min_length[6]',
         'telefone'     => 'required',
         'password_confirmation' => 'required_with[password]|max_length[255]|matches[password]',
@@ -37,6 +37,10 @@ class UsuarioModel extends Model
         'email' => [
             'is_unique' => 'Desculpe. Esse email já está cadastrado.',
             'required' => 'O campo EMAIL é obrigatório.',
+
+        ],
+        'password' => [
+            'required' => 'O campo SENHA é obrigatório.',
 
         ],
         'cpf' => [
