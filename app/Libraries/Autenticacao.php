1327babb462797439a26b3acc1c7861a373c5f8b
Marco Aurélio Silva <coreilabsbr@gmail.com>
2023-11-03 10:57:03 -0300

    aula 59 autenticacao final

--- app/Libraries/Autenticacao.php
+++ app/Libraries/Autenticacao.php
@@ -52,6 +52,65 @@
 
     }
 
+
+    public function logout(){
+
+        session()->destroy();
+    }
+
+    public function pegaUsuarioLogado(){
+
+        // Não esquecer de compartilhar a instância com services
+
+        if($this->usuario === null){
+
+            $this->usuario = $this->pegaUsuarioDaSecao();
+
+        }
+
+        //retornamos o usuario que foi definido no inicio da classe
+        return $this->usuario;
+    }
+
+
+    /**
+     * @descricao: O metodo so permite ficar logado na aplicaçao aquele que ainda existir na base e que esteja ativo. Do contrario, sera feito o logout do mesmo,
+     * caso haja uma mudanca a sua conta durante a sessao
+     * 
+     * @uso no filtro LoginFilter
+     * 
+     * @return retorna true se o metodo pegaUsuarioLogado() nao for null. Ou seja se o usuario estiver logado.
+     */
+
+
+    public function estaLogado(){
+        return $this->pegaUsuarioLogado() != null;
+    }
+
+    private function pegaUsuarioDaSecao(){
+
+
+        if(!session()->has('usuario_id')){
+            return null;
+        }
+        
+        //instanciamos o model usuario
+        $usuarioModel = new App\Models\UsuarioModel();
+        
+        // recupero o usuario de acordo com a chave da sessao 'usuario_id'
+        $usuario = $usuarioModel->find(session()->get('usuario_id'));
+
+
+        // so retorno o objeto usuario se o mesmo for encontrado e estiver ativo
+        if($usuario && $usuario->ativo){
+
+            return $usuario;
+
+
+        }
+    }
+
+
     private function logaUsuario(object $usuario){
 
         $session = session();
