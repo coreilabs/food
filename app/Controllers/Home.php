53f8b59798a8368b7743060498b0af0a0510efe4
Marco Aurélio Silva <coreilabsbr@gmail.com>
2023-11-04 10:13:56 -0300

    aula 75 - email

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -8,4 +8,24 @@ class Home extends BaseController
     {
         return view('welcome_message');
     }
+    public function email(){
+    
+
+        $email = \Config\Services::email();
+
+        $email->setFrom('eldedodeouro@gmail.com', 'Your Name');
+        $email->setTo('eldedodeouro@gmail.com');
+        // $email->setCC('another@another-example.com');
+        // $email->setBCC('them@their-example.com');
+        
+        $email->setSubject('Outro Email');
+        $email->setMessage('Email funcionando.');
+        
+        if($email->send()){
+            echo 'email enviado';
+        }else{
+          echo $email->printDebugger();  
+        }
+
+    }
 }
