cb18a6f09ad3a1f5c5a504ab9581ea6bebcc26c9
Marco Aurélio Silva <coreilabsbr@gmail.com>
2023-11-06 10:20:27 -0300

    aula 85
    crianda migracao Categorias

--- 127_0_0_1.sql
+++ 127_0_0_1.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 02, 2023 at 03:26 PM
+-- Generation Time: Nov 06, 2023 at 02:19 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 7.4.29
 
@@ -24,92 +24,46 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `migrations`
+-- Table structure for table `categorias`
 --
 
-CREATE TABLE `migrations` (
-  `id` bigint(20) UNSIGNED NOT NULL,
-  `version` varchar(255) NOT NULL,
-  `class` varchar(255) NOT NULL,
-  `group` varchar(255) NOT NULL,
-  `namespace` varchar(255) NOT NULL,
-  `time` int(11) NOT NULL,
-  `batch` int(11) UNSIGNED NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `migrations`
---
-
-INSERT INTO `migrations` (`id`, `version`, `class`, `group`, `namespace`, `time`, `batch`) VALUES
-(2, '2023-11-01-122815', 'App\\Database\\Migrations\\CriaTabelaUsuarios', 'default', 'App', 1698844380, 1);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `usuarios`
---
-
-CREATE TABLE `usuarios` (
+CREATE TABLE `categorias` (
   `id` int(5) UNSIGNED NOT NULL,
   `nome` varchar(128) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `cpf` varchar(20) DEFAULT NULL,
-  `telefone` varchar(20) NOT NULL,
-  `is_admin` tinyint(1) NOT NULL DEFAULT 0,
-  `ativo` tinyint(1) NOT NULL DEFAULT 0,
-  `password_hash` varchar(255) NOT NULL,
-  `ativacao_hash` varchar(64) DEFAULT NULL,
-  `reset_hash` varchar(64) DEFAULT NULL,
-  `reset_expira_em` datetime DEFAULT NULL,
+  `slug` varchar(128) NOT NULL,
+  `ativo` tinyint(1) NOT NULL DEFAULT 1,
   `criado_em` datetime DEFAULT NULL,
   `atualizado_em` datetime DEFAULT NULL,
   `deletado_em` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `usuarios`
+-- Dumping data for table `categorias`
 --
 
-INSERT INTO `usuarios` (`id`, `nome`, `email`, `cpf`, `telefone`, `is_admin`, `ativo`, `password_hash`, `ativacao_hash`, `reset_hash`, `reset_expira_em`, `criado_em`, `atualizado_em`, `deletado_em`) VALUES
-(1, 'Marco Aurelio Silva 2', 'admin@gmail.com', '024.566.811-62', '(62) 98206-9063', 1, 1, '', NULL, NULL, NULL, '2023-11-23 17:56:34', '2023-11-02 11:20:04', NULL),
-(2, 'Teste', 'eldedodeouro@gmail.com', '125.093.570-93', '(62) 98534-9760', 1, 1, '', NULL, NULL, NULL, '2023-11-14 00:00:00', '2023-11-02 11:19:28', NULL);
+INSERT INTO `categorias` (`id`, `nome`, `slug`, `ativo`, `criado_em`, `atualizado_em`, `deletado_em`) VALUES
+(1, 'Pizza Salgada', 'pizza-salgada', 1, '2023-11-06 10:16:53', '2023-11-06 10:16:53', NULL);
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `migrations`
+-- Indexes for table `categorias`
 --
-ALTER TABLE `migrations`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `usuarios`
---
-ALTER TABLE `usuarios`
+ALTER TABLE `categorias`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `email` (`email`),
-  ADD UNIQUE KEY `cpf` (`cpf`),
-  ADD UNIQUE KEY `ativacao_hash` (`ativacao_hash`),
-  ADD UNIQUE KEY `reset_hash` (`reset_hash`);
+  ADD UNIQUE KEY `nome` (`nome`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `migrations`
+-- AUTO_INCREMENT for table `categorias`
 --
-ALTER TABLE `migrations`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT for table `usuarios`
---
-ALTER TABLE `usuarios`
-  MODIFY `id` int(5) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+ALTER TABLE `categorias`
+  MODIFY `id` int(5) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
