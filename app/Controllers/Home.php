a452e0ab12293b05382ac13629a1fa7fb9720718
Marco Aurélio Silva <coreilabsbr@gmail.com>
2023-11-21 11:56:53 -0300

    aula 166
    fazendo download e instalando template da area publica

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,26 +6,10 @@ class Home extends BaseController
 {
     public function index(): string
     {
-        return view('welcome_message');
+        $data = [
+            'titulo' => 'Seja muito bem vindo!',
+        ];
+        return view('Home/index', $data);
     }
-    public function email(){
-    
 
-        $email = \Config\Services::email();
-
-        $email->setFrom('eldedodeouro@gmail.com', 'Your Name');
-        $email->setTo('eldedodeouro@gmail.com');
-        // $email->setCC('another@another-example.com');
-        // $email->setBCC('them@their-example.com');
-        
-        $email->setSubject('Outro Email');
-        $email->setMessage('Email funcionando.');
-        
-        if($email->send()){
-            echo 'email enviado';
-        }else{
-          echo $email->printDebugger();  
-        }
-
-    }
 }
