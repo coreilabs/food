c60bcd622d67ac54cba2527fafd471ff97494e74
Marco Aurélio Silva <coreilabsbr@gmail.com>
2023-11-28 18:01:52 -0300

    aula 217

--- app/Views/Carrinho/index.php
+++ app/Views/Carrinho/index.php
@@ -124,8 +124,14 @@
                             </tr>
                             
                         </tbody>
-                        </table>
+                        
                     </table>
+
+                    <div class="form-group col-md-3">
+                        <label>Consulte a Taxa de Entrega</label>
+                        <input type="text" name="cep" class="form-control cep" placeholder="Informe seu CEP" >
+                        <div id="cep"></div>
+                    </div>
               </div>
 
               <hr>
@@ -154,7 +160,42 @@
 
 <?= $this->section('scripts'); ?>
 
+<script src="<?= site_url('admin/vendors/mask/jquery.mask.min.js')?>"></script>
+<script src="<?= site_url('admin/vendors/mask/app.js')?>"></script>
 <script>
+
+    $("[name=cep]").focusout(function(){
+        var cep = $(this).val();
+        if(cep.length === 9){
+
+            $.ajax({
+                type: 'get',
+                url:'<?= site_url('carrinho/consultacep')?>',
+                dataType: 'json',
+                data:{
+                    cep:cep
+                },
+                beforesend: function(){
+                    $("cep").html('Consultando CEP');
+                    $("[name=cep]").val('');
+                },
+                success: function(response){
+                   if(!response.erro){
+                        // cep valido
+
+
+                   }else{
+                    // erro de validacao
+                   }
+                },
+                error:function(){
+                    alert('Não foi possível consultar a taxa de entrega. Por favor entre em contato com nossa equipe e informe o erro: CONSULTA-ERRO-TAXA-ENTREGA');
+                }
+
+            })
+
+        }
+    });
    
 </script>
 <?= $this->endSection(); ?>
